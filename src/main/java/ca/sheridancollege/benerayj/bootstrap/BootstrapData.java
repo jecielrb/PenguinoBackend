02be1a33eefd6a2fe4a2b501815cbeec3a5f843f
domino <akakabeseiji0@gmail.com>
2023-04-07 14:43:21 -0400

    resolve application.properties conflicts

---
+++ src/main/java/ca/sheridancollege/benerayj/bootstrap/BootstrapData.java
@@ -0,0 +1,26 @@
+package ca.sheridancollege.benerayj.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import ca.sheridancollege.benerayj.bean.Pet;
+import ca.sheridancollege.benerayj.repository.PetRepository;
+import lombok.AllArgsConstructor;
+
+@Component 
+@AllArgsConstructor
+public class BootstrapData implements CommandLineRunner {
+
+	private PetRepository petRepo; 
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Pet sample = Pet.builder().name("Sample Pet").age(5).personality("Happy").build();
+		
+		petRepo.save(sample);
+
+	}
+
+}
