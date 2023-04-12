82daa3b4c447f791bb08f6768c213ad5185b6f74
Seiji <akakabeseiji0@gmail.com>
2023-04-12 13:53:48 -0400

    Did some refactoring and documentation (#2)
    * add a get request version of name suggestion. Also added a base64 encoded apikey. delete later when done"
    
    * made things work on my end
    
    * Please dont remove flapdoodle. Its for local debugging of mongodb
    
    * mostly refactoring and documentation

--- src/main/java/ca/sheridancollege/benerayj/services/BotServiceImpl.java
+++ src/main/java/ca/sheridancollege/benerayj/services/BotServiceImpl.java
@@ -1,27 +1,31 @@
 package ca.sheridancollege.benerayj.services;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
 import ca.sheridancollege.benerayj.bean.BotRequest;
 import ca.sheridancollege.benerayj.configuration.ChatGptConfig;
 import ca.sheridancollege.benerayj.requests.ChatGptRequest;
 import ca.sheridancollege.benerayj.requests.ChatGptResponse;
 
-
 @Service
 public class BotServiceImpl {
-
 	private static RestTemplate restTemplate = new RestTemplate();
 	
 	public HttpEntity<ChatGptRequest> buildHttpEntity(ChatGptRequest chatRequest) {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.parseMediaType(ChatGptConfig.MEDIA_TYPE));
-		headers.add(ChatGptConfig.AUTHORIZATION, ChatGptConfig.BEARER + ChatGptConfig.API_KEY);
+		// headers.add(ChatGptConfig.AUTHORIZATION, ChatGptConfig.BEARER + ChatGptConfig.API_KEY);
+
+		// Please replace the 2 following lines with the commented line above this once we are about 
+		// to make this repo public or once we figured out how to manage keys.
+		String apiKey = new String(Base64.decodeBase64(ChatGptConfig.API_KEY_64));
+		headers.add(ChatGptConfig.AUTHORIZATION, ChatGptConfig.BEARER + " " + apiKey);
+		
 		return new HttpEntity<>(chatRequest, headers);
 	}
 	
@@ -45,5 +49,4 @@ public class BotServiceImpl {
 							ChatGptConfig.TEMPERATURE,
 							ChatGptConfig.TOP_P)));
 	}
-	
 }
